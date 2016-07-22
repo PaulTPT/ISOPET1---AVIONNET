49d08465f7e94efb85a45177635da2238391fd1d
PaulTPT <paul.berthier@outlook.com>
2016-07-22 17:41:33 -0400

    Ca avance

--- javascript/module.js
+++ javascript/module.js
@@ -15,82 +15,16 @@
 
     });
 
-    app.controller('EdCriController', function ($scope, EdCriFactory, $routeParams) {
+    app.controller('EdCriController', function ($scope) {
         $scope.fable_id=1;
-        $scope.fable_p=EdCriFactory.getFable_p($scope.fable_id).then(function(fable_p){
-            $scope.fable_p=fable_p;
-        },function(msg){
-            alert(msg);
+        $(document).ready(function(){
+            $('[data-toggle="tooltip"]').tooltip();
         });
     });
 
-    var toXML = function(text){
-        if(window.ActiveXObject)
-        {
-            var doc=new ActiveXObject('Microsoft.XMLDOM');
-            doc.async='false';
-            doc.loadXML(text);
-        }
-        else
-        {
-            var parser=new DOMParser();
-            var doc=parser.parseFromString(text,'text/xml');
-        }
-        return doc;
-    }
 
-
-    var create_nsResolver = function (element) {
-        var
-            nsResolver = element.ownerDocument.createNSResolver(element),
-            defaultNamespace = element.getAttribute('xmlns');
-
-        return function (prefix) {
-            return nsResolver.lookupNamespaceURI(prefix) || defaultNamespace;
-        };
-    };
-
-
-
-    var ParseFable = function(fable){
-        var fable_xml=toXML(fable);
-
-        var nsResolver = create_nsResolver(fable_xml.documentElement);
-
-        var element = fable_xml.evaluate("//tei:text[@xml:id='fable0001']/tei:body/tei:div[@xml:id='f1-ms-BPL']/tei:div[@type='edition-critique']/tei:div[@type='edition-critique-latin']//tei:l", fable_xml, nsResolver, XPathResult.ANY_TYPE, null );
-        var iter = element.iterateNext ();
-        var result=[];
-        var i =0;
-        while (iter) {
-            result.push(iter.textContent);
-            iter = element.iterateNext ();
-            i++;
-        }
-        return result;
-    };
-
-    app.factory('EdCriFactory', function($http, $q){
-        var factory = {
-            getFable_p: function (fable_id) {
-                if(factory.fable_id==fable_id){
-                    return factory.fable_p;
-                }else{
-                    var deferred =$q.defer();
-                    $http.get('resources/Isopet-codage-fable-' + fable_id +'.xml')
-                        .success(function(data, status){
-                            factory.fable_p=ParseFable(data);
-                            deferred.resolve(factory.fable_p);
-                        }).error(function(data, status){
-                            deferred.reject('Impossible de récupérer la fable '+fable_id);
-                    });
-                    return deferred.promise;
-                }
-            },
-            fable_p: false,
-            fable_id: 0,
-        };
-        return factory;
-
+    app.controller('ImageController', function ($scope) {
+        $scope.fable_id=1;
     });
 
 
--- javascript/sidebar.js
+++ javascript/sidebar.js
@@ -2,29 +2,36 @@
  * Created by paultpt on 29/01/16.
  */
 
-$(".menu-toggle-2").click(function(e) {
-    e.preventDefault();
-    $("#wrapper").toggleClass("toggled-2");
-    $('#menu ul').hide();
-});
 
 function initMenu() {
     $('#menu ul').hide();
     $('#menu ul').children('.current').parent().show();
-    //$('#menu ul:first').show();
     $('#menu li a').click(
         function() {
             var checkElement = $(this).next();
             if((checkElement.is('ul')) && (checkElement.is(':visible'))) {
+                checkElement.slideUp('normal');
+                $('#sidebar-wrapper').toggleClass('open',false);
                 return false;
             }
             if((checkElement.is('ul')) && (!checkElement.is(':visible'))) {
                 $('#menu ul:visible').slideUp('normal');
                 checkElement.slideDown('normal');
+                $('#sidebar-wrapper').toggleClass('open',true);
                 return false;
             }
         }
     );
+    $('#menu').hover(
+        function() {
+            return false;
+        },
+        function() {
+            $('#menu ul:visible').slideUp('normal');
+            $('#sidebar-wrapper').toggleClass('open',false);
+            return false;
+        });
+
 }
 
 $(document).ready(function() {
