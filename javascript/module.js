9b8f3ca500d68778c330ce736b6ab54c24cee445
PaulTPT <paul.berthier@outlook.com>
2016-08-21 16:33:39 -0400

    selection des fables'

--- javascript/module.js
+++ javascript/module.js
@@ -3,147 +3,199 @@
  */
 
 
-    var app = angular.module("joanaApp", ['ngRoute']);
+var app = angular.module("joanaApp", ['ngRoute']);
 
-    app.config(function ($routeProvider) {
-        $routeProvider
-            .when('/projet/', {templateUrl: 'partials/projet/projet.html'})
-            .when('/corpus/', {templateUrl: 'partials/corpus/contenu.html', controller: 'CorpusController'})
-            .when('/outils/', {templateUrl: 'partials/outils/outils.html'})
-            .when('/bibliographie/', {templateUrl: 'partials/bibliographie/bibliographie.html'})
-            .otherwise({redirectTo: '/projet'});
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when('/projet/', {templateUrl: 'partials/projet/projet.html'})
+        .when('/corpus/', {templateUrl: 'partials/corpus/contenu.html', controller: 'CorpusController'})
+        .when('/outils/', {templateUrl: 'partials/outils/outils.html'})
+        .when('/bibliographie/', {templateUrl: 'partials/bibliographie/bibliographie.html'})
+        .otherwise({redirectTo: '/projet'});
 
+});
+
+app.controller('BarController', function ($scope, FableFactory) {
+    $scope.$watch(function(){
+        return FableFactory.fable_num;
+    }, function(NewValue, OldValue){
+        fable_id = FableFactory.getFable_id();
+        $scope.fable_name = FableFactory.getFable_title();
     });
+    $scope.category = 'BPL';
+    $scope.subcategory = 'P';
 
+    $scope.next_fable = function () {
+        FableFactory.next_fable();
+    };
 
+    $scope.prev_fable = function () {
+        FableFactory.prev_fable();
+    };
 
-    app.factory('edCriFactory', function(){
-        var factory = {
-            nav_selected : null,
-            select_nav: function(name){
-            factory.nav_selected=name;
-            },
-        };
-        return factory;
+});
 
-    });
+app.factory('FableFactory', function ($http) {
 
-    app.controller('navController', function($scope,edCriFactory) {
-        $scope.setNav = function (nav) {
-            edCriFactory.select_nav(nav);
-        };
-    });
-
-    app.controller('EdCriController', function ($scope,edCriFactory) {
-        $scope.fable_id=1;
-        $scope.$on('$includeContentLoaded', function(event,src) {
-            $('[data-toggle="tooltip"]').tooltip();
-            if(src=='partials/fables/f1-BPL-ed-crit-lat.htm'){
-                $(".zb").zbox();
+    var factory = {
+        num_fables: 2,
+        fable_num: -1,
+        fables: null,
+        next_fable: function () {
+            factory.fable_num = (factory.fable_num + 1) % factory.num_fables;
+        },
+        prev_fable: function () {
+            factory.fable_num = (((factory.fable_num - 1) % factory.num_fables) + factory.num_fables) % factory.num_fables;
+        },
+        getFable_id: function () {
+            if(factory.fable_num==-1){
+                return "Null"
+            }else{
+                return factory.fables[factory.fable_num].id;
             }
-        });
+        },
+        getFable_title: function () {
+            if(factory.fable_num==-1){
+                return "Null"
+            }else{
+                return factory.fables[factory.fable_num].title;
+            }
+        },
+    };
+
+    $http.get('resources/index.json').success(function (data) {
+        factory.fables = data.fables;
+        factory.fable_num=0;
     });
+    return factory;
+});
 
+app.factory('edCriFactory', function () {
+    var factory = {
+        nav_selected: null,
+        select_nav: function (name) {
+            factory.nav_selected = name;
+        }
+    };
+    return factory;
 
-    app.controller('ImageController', function ($scope) {
-        $scope.fable_id=1;
+});
+
+app.controller('navController', function ($scope, edCriFactory) {
+    $scope.setNav = function (nav) {
+        edCriFactory.select_nav(nav);
+    };
+});
+
+app.controller('EdCriController', function ($scope, FableFactory) {
+    $scope.fable_id = 1;
+    $scope.$on('$includeContentLoaded', function (event, src) {
+        $('[data-toggle="tooltip"]').tooltip();
+        if (src == 'partials/fables/f1-BPL-ed-crit-lat.htm') {
+            $(".zb").zbox();
+        }
     });
+});
 
-    app.controller('TransController', function ($scope) {
-        $scope.fable_id=1;
-        $scope.$on('$includeContentLoaded', function(event,src) {
-            if(src=='partials/fables/f1-P-frm-transcription.htm') {
-                $('.slick').slick({
-                    slide: 'div',
-                    dots: true
+
+app.controller('ImageController', function ($scope) {
+    $scope.fable_id = 1;
+});
+
+app.controller('TransController', function ($scope) {
+    $scope.fable_id = 1;
+    $scope.$on('$includeContentLoaded', function (event, src) {
+        if (src == 'partials/fables/f1-P-frm-transcription.htm') {
+            $('.slick').slick({
+                slide: 'div',
+                dots: true
 
             });
-            }
+        }
+        $(".slick-current .im_fable").elevateZoom({
+            scrollZoom: true,
+            zoomType: "inner",
+            cursor: "crosshair"
+        });
+        $('.slick').on('afterChange', function (slick, currentSlide) {
+            $('#zoomed img').removeData('elevateZoom');
+            $('.zoomWrapper img.zoomed').unwrap();
+            $('.zoomContainer').remove();
             $(".slick-current .im_fable").elevateZoom({
-                scrollZoom : true,
-                zoomType	: "inner",
+                scrollZoom: true,
+                zoomType: "inner",
                 cursor: "crosshair"
-            });;
-            $('.slick').on('afterChange', function(slick, currentSlide){
-                $('#zoomed img').removeData('elevateZoom');
-                $('.zoomWrapper img.zoomed').unwrap();
-                $('.zoomContainer').remove();
-                $(".slick-current .im_fable").elevateZoom({
-                    scrollZoom : true,
-                    zoomType	: "inner",
-                    cursor: "crosshair"
-                });
             });
         });
     });
+});
 
 
+app.controller('HeaderController', function ($scope) {
 
-    app.controller('HeaderController', function ($scope) {
+    $scope.desc = "";
 
+    $scope.showDesc = function (text) {
+        $scope.desc = text;
+    };
+
+    $scope.clearDesc = function () {
         $scope.desc = "";
+    };
 
-        $scope.showDesc = function (text) {
-            $scope.desc = text;
-        };
+    $scope.desactive = function () {
+        $scope.dp = false;
+        $scope.af = false;
+        $scope.cp = false;
+        $scope.bb = false;
+    };
 
-        $scope.clearDesc = function () {
-            $scope.desc = "";
-        };
+    $scope.activedp = function () {
+        $scope.desactive();
+    };
 
-        $scope.desactive = function () {
-            $scope.dp = false;
-            $scope.af = false;
-            $scope.cp = false;
-            $scope.bb = false;
-        };
+    $scope.activeaf = function () {
+        $scope.desactive();
+        $scope.af = true;
+    };
 
-        $scope.activedp = function () {
-            $scope.desactive();
-         };
+    $scope.activecp = function () {
+        $scope.desactive();
+        $scope.cp = true;
+    };
 
-        $scope.activeaf = function () {
-            $scope.desactive();
-            $scope.af = true;
-        };
+    $scope.activebb = function () {
+        $scope.desactive();
+        $scope.bb = true;
+    };
 
-        $scope.activecp = function () {
-            $scope.desactive();
-            $scope.cp = true;
-        };
-
-        $scope.activebb = function () {
-            $scope.desactive();
-            $scope.bb = true;
-        };
-
-        $scope.activedp();
+    $scope.activedp();
 
 
-    });
+});
 
-    app.controller('CorpusController', function ($scope) {
+app.controller('CorpusController', function ($scope) {
 
-        $scope.request = "ed_cri";
+    $scope.request = "ed_cri";
 
-        $scope.setRequest = function (req) {
-            $scope.request = req;
-        };
+    $scope.setRequest = function (req) {
+        $scope.request = req;
+    };
 
-    });
+});
 
 
-app.directive('highlight', ['edCriFactory', function(edCriFactory){
+app.directive('highlight', ['edCriFactory', function (edCriFactory) {
     return {
-        restrict : 'A',
-        link : function(scope, element, attr){
-            scope.$watch(function(){
+        restrict: 'A',
+        link: function (scope, element, attr) {
+            scope.$watch(function () {
                 return edCriFactory.nav_selected;
-            }, function(newValue, oldValue) {
-                if(attr.highlight.indexOf(newValue)>=0){
-                   element.toggleClass('highlight',true);
-                }else{
-                    element.toggleClass('highlight',false);
+            }, function (newValue, oldValue) {
+                if (attr.highlight.indexOf(newValue) >= 0) {
+                    element.toggleClass('highlight', true);
+                } else {
+                    element.toggleClass('highlight', false);
                 }
             });
 
---
+++ resources/index.xsl
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="utf-8" ?>
+<xsl:stylesheet version="2.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+  xmlns:xs="http://www.w3.org/2001/XMLSchema"
+  xmlns:tei="http://www.tei-c.org/ns/1.0"
+  exclude-result-prefixes="xs">
+
+<xsl:output method="text" indent="yes"/>
+
+ <xsl:namespace-alias result-prefix="#default" stylesheet-prefix="tei"/>
+ <xsl:strip-space elements="lem rdg l"/>
+
+  <xsl:template match="/">
+        <xsl:text>{"fables":[&#xa;</xsl:text>
+        <xsl:apply-templates select="//tei:text" />
+        <xsl:text>]}&#xa;</xsl:text>
+  </xsl:template>
+
+  <xsl:template match="tei:text">
+      <xsl:text>&#x9;{"id":"</xsl:text>
+      <xsl:value-of select="@n"/>
+      <xsl:text>",</xsl:text>
+      <xsl:apply-templates select="descendant::tei:head[@type='titre-La-Fontaine']"/>
+      <xsl:text>,&#xa;</xsl:text>
+  </xsl:template>
+
+  <xsl:template match="tei:text[position() = last()]">
+      <xsl:text>&#x9;{"id":"</xsl:text>
+      <xsl:value-of select="@n"/>
+      <xsl:text>",</xsl:text>
+      <xsl:apply-templates select="descendant::tei:head[@type='titre-La-Fontaine']"/>
+      <xsl:text>&#xa;</xsl:text>
+  </xsl:template>
+  
+  
+  
+<xsl:template match="tei:head">
+  <xsl:text> "title":"</xsl:text>
+  <xsl:value-of select="."/>
+  <xsl:text>"}</xsl:text>
+</xsl:template>
+  
+</xsl:stylesheet>
